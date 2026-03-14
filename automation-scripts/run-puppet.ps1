c293bd80da3b8f77cfdd6fb4e8988850b69fa435
Shivanshu <shivanshu25@gmail.com>
2026-03-14 23:40:10 +0530

    Added automation script and puppet module

---
+++ automation-scripts/run-puppet.ps1
@@ -0,0 +1,5 @@
+Write-Host "Starting Puppet Configuration..."
+
+puppet apply puppet-system/main.pp
+
+Write-Host "Configuration Completed Successfully."
---
+++ config-modules/webserver/manifests/init.pp
@@ -0,0 +1,12 @@
+class webserver {
+
+  package { 'apache2':
+    ensure => installed,
+  }
+
+  service { 'apache2':
+    ensure => running,
+    enable => true,
+  }
+
+}
